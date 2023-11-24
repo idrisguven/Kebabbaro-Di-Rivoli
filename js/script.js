c922d554ca86e0f2594f5418216f2142ca8741cb
idrisguven <idrisjob46@gmail.com>
2023-11-24 16:10:57 +0100

    animated x buton

--- js/script.js
+++ js/script.js
@@ -3,11 +3,10 @@ let menu = document.querySelector("#menu");
 
 menuBtn.addEventListener("click", function () {
    menu.classList.toggle("active");
+   menuBtn.classList.toggle("active");
 });
 
 window.onscroll = () => {
    menu.classList.remove("active");
+   menuBtn.classList.remove("active");
 };
-
-
-
