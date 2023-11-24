b0e29668417f7897241ded6dc588c0e624670e2f
idrisguven <idrisjob46@gmail.com>
2023-11-24 17:46:11 +0100

    scroll added

---
+++ js/scrollSource.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  // Mostra o nasconde il pulsante in base allo scroll
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  }
+
+  // Funzione per lo scroll verso l'alto
+  function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+  // Aggiungi un gestore di eventi al pulsante
+  scrollToTopBtn.addEventListener("click", scrollToTop);
+});
